cfb18c500a5d22eb722da760ba381905c42cbadc
ecsanchesjr <ecsanchesjr@hotmail.com>
2017-04-20 19:28:37 -0300

    Organização em pastas do projeto, criação da index e do CSS common

---
+++ Web/controller/constant_states.php
@@ -0,0 +1,4 @@
+<?php
+	define('ESTADO_EMPRESTADO', '0');
+	define('ESTADO_DEVOLVIDO', '1');
+?>
---
+++ Web/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>SACI - Sistema Automatizado de Controle de Inventário</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="./view/css/common.css" />
+	</head>
+	<body>
+		<p  class="title low-title" align="center"> Sistema Automatizado de Controle de Inventário </p>
+	</body>
+</html>
