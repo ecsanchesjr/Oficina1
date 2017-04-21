a7fab5d3bc6d5a18f136f3983985c5e8e25afcbc
ecsanchesjr <ecsanchesjr@hotmail.com>
2017-04-21 14:02:50 -0300

    Arrumar erro de cache

--- Web/index.php
+++ Web/index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta http-equiv="pragma" content="no-cache" /> <!-- Limpar o cache quando inicia a página -->
 		<title>SACI - Sistema Automatizado de Controle de Inventário</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="./view/css/bootstrap.min.css" />
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" type="text/css" href="./view/css/common.css" />
 		<link rel="stylesheet" type="text/css" href="./view/css/index.css" />
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	</head>
 
 	<body>
