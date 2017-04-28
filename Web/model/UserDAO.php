1cce9929a8924fa33e26f641eb9d40f60510f636
ecsanchesjr <ecsanchesjr@hotmail.com>
2017-04-28 14:56:14 -0300

    Criação de Home básica

--- Web/model/UserDAO.php
+++ Web/model/UserDAO.php
@@ -17,13 +17,13 @@ class UserDAO{
 	}
 
 	function verifUserExists($user, $pass){
-		$search = "SELECT usuario_senha FROM usuario WHERE usuario_nick =";
+		$search = "SELECT usuario_passwd FROM usuario WHERE usuario_nick =";
 		$search .= "'$user';";
 
 		$result = $this->connect->query($search);
 		if($result->num_rows == 1){   // verifica se encontrou algum usuário de acordo com o dado passado
 			$RowsData = $result->fetch_assoc();
-			if($pass == $RowsData['usuario_senha']){  // testa a senha para ver se esta igual
+			if($pass == $RowsData['usuario_passwd']){  // testa a senha para ver se esta igual
 				return(true);   //TUDO DEU CERTO
 			}else{
 				return(false);  // SENHA INVÁLIDA
---
+++ Web/view/Home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="css/common.css" />
+		<link rel="stylesheet" type="text/css" href="css/home.css" />
+	</head>
+	<body>
+		<div class="infoGroup boxInfos">
+			<p class="textInfos">
+				Elcio Cezaeio
+			</p>
+		</div>
+	</body>
+</html>
