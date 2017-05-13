067beeed9ae159dda003cb76a7dfdde7f5009980
ecsanchesjr <ecsanchesjr@hotmail.com>
2017-05-13 18:49:46 -0300

    Alterando as referências de JS e CSS

--- Web/view/Home.php
+++ Web/view/Home.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 		<title>SACI - Área de usuários</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="css/common.css" />
 		<link rel="stylesheet" type="text/css" href="css/home.css" />
 		</head>
